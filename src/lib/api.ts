7ac6eafeec28ae17ed636d45f338ff0bf7367a49
Madhiraju Bharghav <bhargavmadhiraju123@gmail.com>
2026-07-20 14:57:47 +0530

    changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8001",
+  // baseURL: "http://127.0.0.1:8001",
+  baseURL: "https://coral-app-8t2db.ondigitalocean.app",
 });
