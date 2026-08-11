86770a880efae44aaabe3d7505511b3a851c89b5
bharg <bharg@example.com>
2026-08-11 16:18:04 +0530

    changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,14 +1,6 @@
-//import axios from "axios";
-
-//export const api = axios.create({
-  // baseURL: "http://127.0.0.1:8001",
-  //baseURL: "https://www.nxzenai.com",
-//});
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_STUDIO_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // baseURL: "http://127.0.0.1:8001",
+  baseURL: "https://www.nxzenai.com",
 });
