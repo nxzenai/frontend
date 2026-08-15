683ef801d7b4b1f88f97f02e7a579491cd07110b
bharg <bharg@example.com>
2026-08-15 06:20:02 +0530

    changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,9 +4,6 @@ export const api = axios.create({
    //baseURL: "http://127.0.0.1:8001",
   //baseURL: "https://www.nxzenai.com",
   baseURL: "https://coral-app-8t2db.ondigitalocean.app",
-  headers: {
-    "Content-Type": "application/json",
-  },
 
 });
 
