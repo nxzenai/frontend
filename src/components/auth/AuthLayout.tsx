5fa04e0f4f5bd9ed0d67eb21189211e7c06f5ad0
BHARGHAV MADHIRAJU <bhargavmadhiraju123@gmail.com>
2026-08-20 12:39:10 +0530

    changes

--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -82,11 +82,11 @@ export default function AuthLayout({
             <div className="rounded-xl border border-slate-800 bg-slate-900/70 p-6">
 
               <h3 className="mb-2 font-bold text-white">
-                Agentic AI
+                AutoDL / AutoNLP
               </h3>
 
               <p className="text-sm text-slate-400">
-                Enterprise AI Agents.
+              No-code Deep Learning and Natural Language Proceessing.
               </p>
 
             </div>
