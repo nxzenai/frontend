437617bf859349e625f3f8352deaa52db412bebe
bharg <bharg@example.com>
2026-08-27 21:15:28 +0530

    changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-   baseURL: "http://127.0.0.1:8001",
+   //baseURL: "http://127.0.0.1:8001",
   //baseURL: "https://www.nxzenai.com",
-  //baseURL: "https://coral-app-8t2db.ondigitalocean.app",
+  baseURL: "https://coral-app-8t2db.ondigitalocean.app",
 
 });
 
